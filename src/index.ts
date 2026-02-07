2bf1621de3dd8a7ebb7231abefe5ca41aa30c00d
karimtmagdy <karimtmagdy@gmail.com>
2026-02-07 16:26:01 +0200

    update file vercel

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
+import express from "express";
 import "dotenv/config";
-import app from "./app";
 import { Database } from "./config/data/db";
-const PORT = process.env.PORT || 8000;
 
-void (async () => {
+import { setupRoutes } from "./routes";
+
+const app = express();
+
+// Initialize Database with error handling
+const initDb = async () => {
   try {
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
     await Database.getInstance();
+    console.log("✅ Database initialized successfully");
   } catch (error) {
-    console.log("Server connection error", error);
+    console.error("❌ Database initialization failed:", error);
   }
-})();
+};
+
+initDb();
+
+app.get(["/", "/health"], (_req, res) => {
+  res.status(200).json({
+    code: 200,
+    status: "success",
+    api: "Mini Amazon",
+    message: "Welcome to the Vercel Backend",
+    developer: "karimtmagdy",
+    platform: "Vercel",
+    version: "v1",
+    environment: "production",
+  });
+});
+setupRoutes(app);
+
+export default app;
