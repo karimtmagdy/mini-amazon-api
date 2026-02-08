2fac1cb0e575eb34f931f50ac3ebaddc5095c9d0
karimtmagdy <karimtmagdy@gmail.com>
2026-02-08 02:30:59 +0200

    fix

--- src/lib/cookie-options.ts
+++ src/lib/cookie-options.ts
@@ -3,7 +3,7 @@ import { env } from "./env";
 
 export const CookieOptions: ExpressCookieOptions = {
   httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
-  secure: env.nodeEnv === "production", // Only send cookie over HTTPS in production
+  secure: true, //env.nodeEnv === "production", // Only send cookie over HTTPS in production
   sameSite: "none", // Lax in dev, None in prod for cross-domain
   // sameSite: env.nodeEnv === "production" ? "none" : "lax", // Lax in dev, None in prod for cross-domain
   maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
