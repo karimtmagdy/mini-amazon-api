28954da413202ce8261076fa7bfec4a209d72259
karimtmagdy <karimtmagdy@gmail.com>
2026-02-09 09:15:22 +0200

    Feat: Add user profile routes and deactivate account

--- src/routes/public/user.public.routes.ts
+++ src/routes/public/user.public.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from "express";
 import { userController } from "../../controllers/user.controller";
 import { authenticated } from "../../middlewares/authroized";
+import { updateProfileZod } from "../../schemas/user.schema";
+import { validate } from "../../middlewares/validate";
+import { idParamSchema } from "../../schemas/standred.schema";
+import { deactivateUserSchema } from "../../schemas/auth.schema";
 
 const router = Router();
 
 router.use(authenticated);
-router.route("/me").get(userController.getUserHimself);
+router
+  .route("/me")
+  .get(userController.getUserHimself)
+  .patch(validate(updateProfileZod), userController.updateUserHimself)
+  .delete(validate(idParamSchema), userController.deleteHimself);
+
+router.patch(
+  "/me/deactivate",
+  validate(deactivateUserSchema),
+  userController.deactivateAccount,
+);
 
 export default {
   path: "/users",
