c7c8289451846b48b99018cdcaee5640ebdf30b6
karimtmagdy <karimtmagdy@gmail.com>
2026-02-13 03:25:43 +0200

    update route state

--- src/routes/admin/stats.admin.routes.ts
+++ src/routes/admin/stats.admin.routes.ts
@@ -1,27 +1,26 @@
 import { Router } from "express";
 
 const router = Router();
-const { route } = router;
 
-route("/products").get((req, res) => {
+router.route("/products").get((req, res) => {
   res.json({ message: "Admin products" });
 });
-route("/overview").get((req, res) => {
+router.route("/overview").get((req, res) => {
   res.json({ message: "Admin overview" });
 });
-route("/analysis").get((req, res) => {
+router.route("/analysis").get((req, res) => {
   res.json({ message: "Admin analysis" });
 });
-route("/settings").get((req, res) => {
+router.route("/settings").get((req, res) => {
   res.json({ message: "Admin settings" });
 });
-route("/sales").get((req, res) => {
+router.route("/sales").get((req, res) => {
   res.json({ message: "Admin sales" });
 });
-route("/users").get((req, res) => {
+router.route("/users").get((req, res) => {
   res.json({ message: "Admin users" });
 });
-route("/users-activity").get((req, res) => {
+router.route("/users-activity").get((req, res) => {
   res.json({ message: "Admin users activity" });
 });
 
